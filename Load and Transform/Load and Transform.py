c742f78c8d219ddc4f5280cd9f3715e967322207
Rohit Shaw <40485926+shaw-rohit@users.noreply.github.com>
2019-09-25 15:16:47 +0200

    read data and add columns

--- Load and Transform/Load and Transform.py
+++ Load and Transform/Load and Transform.py
@@ -4,3 +4,13 @@ import pandas as pd
 # COMMAND ----------
 
 df = pd.read_csv("/mnt/group17/flickr/yfcc100m_dataset-0.bz2", sep="\t")
+df.columns = ["Photo/video identifier","User NSID","User nickname","Date taken","Date uploaded","Capture device","Title",
+           "Description","User tags (comma-separated)","Machine tags (comma-separated)","Longitude","Latitude","Accuracy",
+           "Photo/video page URL","Photo/video download URL","License name","License URL","Photo/video server identifier",
+           "Photo/video farm identifier","Photo/video secret","Photo/video secret original","Photo/video extension original",
+           "Photos/video marker (0 = photo, 1 = video)"]
+
+# COMMAND ----------
+
+pd.set_option('display.max_colwidth', 400)
+df.head(10)
